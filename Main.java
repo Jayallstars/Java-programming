e138312b8fb7df450bfc05eea3a764c5a254a16e
Jayallstars <jayza0072555@gmail.com>
2022-01-03 10:29:36 +0700

    Second

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
-import java.util.Arrays;
-
 public class Main {
         public static void main(String[] args) {
-                int[][] numbers = new int[2][3];
-                numbers[0][0] = 1;
-                numbers[0][1] = 1;
-                numbers[0][2] = 2;
-                System.out.println(Arrays.deepToString(numbers));
+                System.out.println("Hello World");
         }  
 }
