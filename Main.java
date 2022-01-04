38ad443e2c3bbcbfd6d8f8f7f5599e4cec7247ea
Jayallstars <jayza0072555@gmail.com>
2022-01-04 22:10:12 +0700

    random number

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
         public static void main(String[] args) {
-                System.out.println("Hello World");
+                int randomNum = (int)(Math.random() * 200);
+                System.out.println(randomNum);
         }  
 }
